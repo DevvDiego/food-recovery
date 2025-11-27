52eb2d3ac00ecfbe761f568c2b4f4d0a26d03f16
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-11-27 01:40:10 -0600

    Add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,8 +7,33 @@
 
 <svelte:head>
 	<link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
-	<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+
+<div class="navbar bg-base-100 shadow-sm">
+	<div class="flex-1">
+		<a class="btn btn-ghost text-xl">Empresa</a>
+	</div>
+
+	<div class="flex-none">
+	
+		<ul class="menu menu-horizontal px-1">
+			<li><a>Link</a></li>
+			<li>	
+				<details>
+					<summary>Menu</summary>
+					<ul class="bg-base-100 rounded-t-none p-2">
+						<li><a>Link 1</a></li>
+						<li><a>Link 2</a></li>
+					</ul>
+				</details>
+			</li>
+		</ul>
+	
+	</div>
+</div>
+
+
 
 {@render children()}
