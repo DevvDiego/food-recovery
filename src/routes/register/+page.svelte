1fdf8e0ebca65ac02d4e9e3f4c1b3f7a44a6bde9
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-11-27 11:54:15 -0600

    Add basic register form

---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,14 @@
+<div class="m-auto max-w-md min-h-svh">
+    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-sm border p-4">
+        <legend class="fieldset-legend text-lg">Registro</legend>
+
+        <label class="label">Nombre</label>
+        <input type="text" class="input" placeholder="John Doe" />
+
+        <label class="label">Numero de telefono</label>
+        <input type="text" class="input" placeholder="000 000 0000" />
+
+        <label class="label">Email</label>
+        <input type="email" class="input" placeholder="Name" />
+    </fieldset>
+</div>
