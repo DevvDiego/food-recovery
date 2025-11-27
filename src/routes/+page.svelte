25ea0312a78745b623541d81e47fc9b1e0ceee94
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-11-27 02:41:57 -0600

    Change CTA buttons first appearance

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,3 @@
-<div class="hero bg-base-200 min-h-screen">
-
-    <div class="hero-content text-center">
-
-        <div class="max-w-md">
-
-            <h1 class="text-5xl md:text-6xl font-bold">Aprovechemos al maximo</h1>
-            <p class="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                quasi. In deleniti eaque aut repudiandae et a id nisi.
-            </p>
-            <button class="btn btn-primary">Empecemos</button>
-
-        </div>
-
-    </div>
-
-</div>
-
-
-<div class="text-center my-10">
-    <span class="text-rotate text-5xl md:text-7xl">
-        <span class="justify-items-center">
-            <span>AHORRA</span>
-            <span>CONSUME</span>
-            <span>APROVECHA</span>
-        </span>
-    </span>
-</div>
-
-
 <div class="hero bg-base-200 min-h-screen md:px-10">
   <div class="hero-content flex-col lg:flex-row-reverse">
     
@@ -39,7 +8,7 @@
 
     <div>
 
-        <h1 class="text-5xl font-bold">¿Por que hacemos esto?</h1>
+        <h1 class="text-5xl font-bold">¿Por que hacemos esto? (-----?)</h1>
         <p class="py-6 pr-6">
             Diario muchos negocios de comida se ven obligados a tirar a la basura
             los alimentos que no consiguieron venderse durante el dia. ¿Por que no mejor
@@ -58,4 +27,34 @@
 </div>
 
 
+<div class="text-center my-10">
+    <span class="text-rotate text-5xl md:text-7xl">
+        <span class="justify-items-center">
+            <span>AHORRA</span>
+            <span>CONSUME</span>
+            <span>APROVECHA</span>
+        </span>
+    </span>
+</div>
+
+
+
+<div class="hero bg-base-200 min-h-screen">
+
+    <div class="hero-content text-center">
+
+        <div class="max-w-md">
+
+            <h1 class="text-5xl md:text-6xl font-bold">Aprovecha al maximo</h1>
+            <p class="py-6">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                quasi. In deleniti eaque aut repudiandae et a id nisi. (-----?)
+            </p>
+            <button class="btn btn-primary">Empecemos</button>
+
+        </div>
+
+    </div>
+
+</div>
 
