5cf28f8d30ec0797cab5d791a1704d6a720bbc82
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-11-27 02:14:43 -0600

    Add basic fill info for structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="hero bg-base-200 min-h-screen">
+
+    <div class="hero-content text-center">
+
+        <div class="max-w-md">
+
+            <h1 class="text-5xl md:text-6xl font-bold">Aprovechemos al maximo</h1>
+            <p class="py-6">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+            <button class="btn btn-primary">Empecemos</button>
+
+        </div>
+
+    </div>
+
+</div>
+
+
+<div class="text-center my-10">
+    <span class="text-rotate text-5xl md:text-7xl">
+        <span class="justify-items-center">
+            <span>AHORRA</span>
+            <span>CONSUME</span>
+            <span>APROVECHA</span>
+        </span>
+    </span>
+</div>
+
+
+<div class="hero bg-base-200 min-h-screen md:px-10">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+      class="max-w-sm rounded-lg shadow-2xl"
+    />
+
+    <div>
+
+      <h1 class="text-5xl font-bold">Box Office News!</h1>
+      <p class="py-6 pr-6">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
+
+      <!-- <button class="btn btn-primary">Get Started</button> -->
+
+    </div>
+
+  </div>
+</div>
+
+
+
