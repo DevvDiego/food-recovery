7a79ce9842a07813f6b8bbdc8b424fc02259a8a5
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-12-03 15:07:11 -0600

    nw config

--- svelte.config.js
+++ svelte.config.js
@@ -7,13 +7,9 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'docs',
-			assets: 'docs',
-			fallback: "index.html",
-			precompress: false,
-			strict: true
+			fallback: 'index.html',
+			pages: "docs",
+            assets: "docs"
 		}),
 
 		paths:{
