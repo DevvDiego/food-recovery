175bfa6b8dec8a72e6e1e479d03c7f2865174c11
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-12-03 15:03:09 -0600

    push build

---
+++ docs/_app/immutable/chunks/21R36vkd.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1f5v8pk?.base??"/food-recovery",e=globalThis.__sveltekit_1f5v8pk?.assets??s??"";export{e as a,s as b};
---
+++ docs/_app/immutable/chunks/e6JiTE2k.js
@@ -0,0 +1,1 @@
+import{x as a}from"./C3SM_hji.js";a();
